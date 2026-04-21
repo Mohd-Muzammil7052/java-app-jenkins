a683102f0416c51e1a1cc72c0611d5dbcce30b95
Mohd Muzammil <mohd_muzammil@epam.com>
2026-04-21 22:52:42 +0530

    updated main class

--- src/main/java/org/muzammil/jenkinsjavaapp/JenkinsJavaAppApplication.java
+++ src/main/java/org/muzammil/jenkinsjavaapp/JenkinsJavaAppApplication.java
@@ -2,12 +2,18 @@ package org.muzammil.jenkinsjavaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsJavaAppApplication {
+public class JenkinsJavaAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsJavaAppApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsJavaAppApplication.class, args);
     }
-
 }
